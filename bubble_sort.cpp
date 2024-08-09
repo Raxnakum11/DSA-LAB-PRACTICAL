f53798b1c8b137635533b9557c3894fd6dc124ee
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2024-08-09 22:31:18 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,39 +1,34 @@
 #include<iostream>
 using namespace std;
 
-
-int bubble_sort(int arr[],int r)
-{         int count=0;
-    for(int i=0;i<r;i++){
-
-        for(int j=0;j<r-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
-             count++;
+void bubble_sort(int arr[], int r) {
+    int count;
+    for(int i = 0; i < r; i++) {
+        count = 0;  // Reset count for each pass
+        for(int j = 0; j < r - i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                count++;
             }
         }
-         if(count==0)
-     {
-     cout<<"Array is sorted."<<endl;
-        break;
-     }
+        if(count == 0) {
+            cout << "Array is already sorted." << endl;
+            break;
+        }
     }
 
-  for (int i = 0; i < r; i++) {
-        cout<<arr[i] << " ";
+    for(int i = 0; i < r; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
-
 }
 
+int main() {
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-int main(){
-  int arr[]={1,2,3,4};
-  int n = sizeof(arr) / sizeof(arr[0]);
+    bubble_sort(arr, n);
 
-bubble_sort(arr,n);
-
-
-
- return 0;
+    return 0;
 }
+
